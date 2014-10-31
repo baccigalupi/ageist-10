2d3cee88ebd5c849f30716f416f1805e8eb7a54e
Kane Baccigalupi <baccigalupi@gmail.com>
2014-10-31 15:09:19 -0700

    Feedback

--- ageist.rb
+++ ageist.rb
@@ -1,8 +1,16 @@
-random_age = rand(111)
+random_age = rand(111) # good catch on this edge case
 range_string = "Age range is "
 puts "Age is #{random_age}"
 
-if random_age.between?(0,1)
+# Watch your indentation.
+# It is really important to communication about what 
+# your code is doing. if/elsif/else/end should all be the same indentation
+
+# like the use of between, but do you
+# need to test the bottom of the conditional after the first
+# branch of the if statement? Should be that we don't get into
+# that branch unless we have sorted out the younger ages already
+if random_age.between?(0,1) 
   puts range_string + 'baby'
   elsif random_age.between?(2,9)
     puts range_string + 'child'
